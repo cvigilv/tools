ff7641388a4774319cefff3d4e90925445f29f4e
Carlos Vigil Vásquez <cvigil2@uc.cl>
2019-05-21 15:48:01 -0400

    small changes

--- rmd_preset.sh
+++ rmd_preset.sh
@@ -10,7 +10,7 @@ read author
 echo "Do you need a table of contents? [true / false]"
 read TOC
 
-PRESET="---\ntitle: '$title'\nauthor: '$author'\noutput: pdf_document\nlan: 'es'\ntoc: '$TOC'\nfontsize: 10pt\n---\n\n-----\n\n#"
+PRESET="---\ntitle: '$title'\nauthor: '$author'\noutput:\n    pdf_document:\n        number_sections: true\n        toc: $TOC\nlang: 'es'\nfontsize: 10pt\n---\n\n-----\n\n#"
 
 echo -e $PRESET > $fname.rmd
 echo "Created $frname.rmd in current directory"
